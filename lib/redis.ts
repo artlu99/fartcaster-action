264c9efaa06450fca2d624bca51f0965aadc7086
artlu99 <alu99.sm@gmail.com>
2024-04-04 19:06:43 -0400

    Upstash redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,7 +1,8 @@
-import { Redis } from "ioredis";
+import { Redis } from "@upstash/redis";
 
-export const redis = new Redis(
-  process.env.REDIS_URL ?? "redis://localhost:6379",
-);
+export const redis = new Redis({
+  url: process.env.REDIS_URL ?? "redis://localhost:6379",
+  token: process.env.REDIS_TOKEN ?? ""
+});
 
 export default redis;
