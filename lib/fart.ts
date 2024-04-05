534f03245a687521e34f2f4e9f9dc362edb9b8e4
artlu99 <alu99.sm@gmail.com>
2024-04-05 15:29:20 -0400

    HSET arguments for upstash redis library

--- lib/fart.ts
+++ lib/fart.ts
@@ -3,5 +3,5 @@ import redis from "./redis.js";
 export async function fart(fid: number, username: string) {
   const id = fid.toString();
   await redis.zincrby("farts", 1, id);
-  await redis.hset("usernames", { id: username });
+  await redis.hset("usernames", { [id]: username });
 }
